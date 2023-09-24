29d262293ae645d3c84044be1ebaa61c698ce401
andrey28121989 <anenashev89@gmail.com>
2023-09-24 23:44:57 +0300

    feat: add html and css for player

---
+++ audio-player/playlist.js
@@ -0,0 +1,19 @@
+const playList = [
+  { 
+    title: 'Akvarium - Poezd v ogne',
+    src: './assets/sound/Akvarium - Poezd v ogne.mp3',
+    duration: '1:05'
+  },
+  { 
+    title: 'Animal DzhaZ - CHuvstva',
+    src: './assets/sound/Animal DzhaZ - CHuvstva.mp3',
+    duration: '0:34'
+  },
+  {
+    title: 'Bumboks - Pepel',
+    src: './assets/sound/Bumboks - Pepel.mp3',
+    duration: '0:23'
+  }
+]
+
+  export default playList;
