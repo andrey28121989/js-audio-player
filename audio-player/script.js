67ccfece653846619f918a01b2ee94dc251a2306
andrey28121989 <anenashev89@gmail.com>
2023-09-25 14:20:51 +0300

    feat: add change pictures

--- audio-player/script.js
+++ audio-player/script.js
@@ -3,17 +3,20 @@ const playList = [
   { 
     title: 'Akvarium - Poezd v ogne',
     src: './assets/audio/Akvarium - Poezd v ogne.mp3',
-    duration: '1:05'
+    duration: '1:05',
+    cover: './assets/img/img1.jpg'
   },
   { 
     title: 'Animal DzhaZ - CHuvstva',
     src: './assets/audio/Animal DzhaZ - CHuvstva.mp3',
-    duration: '0:34'
+    duration: '0:34',
+    cover: './assets/img/img2.jpg'
   },
   {
     title: 'Bumboks - Pepel',
     src: './assets/audio/Bumboks - Pepel.mp3',
-    duration: '0:23'
+    duration: '0:23',
+    cover: './assets/img/img3.jpg'
   }
 ]
 
@@ -24,6 +27,9 @@ const playButton = document.querySelector('.play');
 const playPrev = document.querySelector('.play-prev');
 const playNext = document.querySelector('.play-next');
 const playListItem = document.querySelector('.play-list');
+const playerImg = document.querySelector('.player__img');
+const backgroungImg = document.querySelector('.background');
+
 
 playList.forEach(element => {
   const li = document.createElement('li');
@@ -41,6 +47,8 @@ const playAudio = () => {
   if (!isPlay) {
     audio.src = playList[playNum].src;
     audio.currentTime = audioTime;
+    playerImg.src = playList[playNum].cover;
+    backgroungImg.src = playList[playNum].cover;
     audio.play();
     isPlay = true;
     playButton.classList.toggle('pause');
